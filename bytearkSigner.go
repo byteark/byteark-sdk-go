845cb9604c4c9c911e62bf734ddd7df3cf69434d
Woramate Jumroonsilp <woramate@inox.co.th>
2020-09-20 22:48:52 +0700

    Try print

--- bytearkSigner.go
+++ bytearkSigner.go
@@ -2,10 +2,12 @@ package bytearksigner
 
 import (
 	"errors"
+	"fmt"
 )
 
 // CreateSigner create new signer for further use.
 func CreateSigner(options SignerOptions) error {
+	fmt.Println("created")
 	signer := CurrentSigner()
 
 	signer.SetAccessID(options.AccessID)
